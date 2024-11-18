2882baf5f5b60682d44c41807584164fc31d6932
ujjawalbisht <ujjawal.bisht@outlook.com>
2024-11-18 11:00:18 +0530

    18/11/24

--- LAb 8/Insertion.c
+++ LAb 8/Insertion.c
@@ -6,3 +6,43 @@ struct node{
     int data ;
     struct node* next ;
 };
+
+struct node* top = NULL;
+
+void push(int x){
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = x;
+    temp->next = top;
+    top = temp;
+}
+
+void pop(){
+    struct node* temp = top;        
+    top = top->next;
+    free(temp);
+}
+
+void display(){
+    struct node* temp = top;
+    while(temp != NULL){
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Enter how many elements u want to enter:- ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        int x;
+        printf("Enter element %d:- ", i + 1);
+        scanf("%d", &x);
+        push(x);
+    }
+    display();
+    pop();
+    display();
+    return 0;
+}  
